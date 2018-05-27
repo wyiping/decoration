d5110d3cec56bd6451a0175136cfe5a2f10a0fc8
wangyiping <61780374@163.com>
2018-05-27 10:11:24 +0800

    update:更新选材页面

--- src/components/page/choose.vue
+++ src/components/page/choose.vue
@@ -1,5 +1,40 @@
 <template>
   <div>
-      choose
+    <ul class="newsContent animated">
+        <router-link
+          v-for="(val,index) in listCon"
+          :to="{name:'detaile'}"
+          class="newsDetaile"
+          :key="index"
+        >
+        <p class="title">{{val.title}}</p>
+        <div>
+          <img alt="加载出错" v-for="(url,index) in val.img_arr" :key="index" :src="url">
+          <div class="bottomInfo clearfix">
+            <span class="writer">{{val.source}}</span> &nbsp;&nbsp;
+            <span class="comment_count">阅读&nbsp;{{val.views}}</span>
+            <span class="datetime">{{val.date}}</span>
+          </div>
+        </div>
+      </router-link>
+    </ul>
   </div>
 </template>
+<script>
+export default {
+  data() {
+    return {
+      listCon: []
+    };
+  },
+  mounted() {
+    this.$http.get("/api/api/choose").then(({ data }) => {
+      this.listCon = data.list;
+    });
+  }
+};
+</script>
+
+<style lang="less" scoped rel="styleheet/less">
+@import "../../assets/css/style.less";
+</style>
